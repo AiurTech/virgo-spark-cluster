62268bebacb2db3692cee0e1fff3d8bdc60fa786
Fernando Racca <cto@aiur.co.uk>
2019-02-04 07:00:35 +0000

    Hive metastore 1.2.0 (Compatible with Spark 2.x+) installed and schema auto created at startup

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #USER Config 
-VERSION=0.2.6
+VERSION=0.2.8
 NETWORK_NAME=virgo
 NETWORK_SUBNET=172.15.0.0/16
 
 # Defaults
 REPO=aiurtech
 base_containers=(virgo-base hadoop-base)
-containers=(hadoop-namenode hadoop-datanode hive-metastore-postgresql)
+containers=(hadoop-namenode hadoop-datanode hive-metastore-postgresql hive-metastore)
 declare -a all_containers=(${base_containers[@]} ${containers[@]})
--- hive-metastore-postgresql/init-hive-db.sh
+++ hive-metastore-postgresql/init-hive-db.sh
@@ -2,15 +2,13 @@
 set -e
 
 echo "Initializing Postgres DB metastore with user ${POSTGRES_USER} on Schema 'public'"
-echo "Creating DB user hive and granting Privileges"
+echo "Creating DB user hive and granting required privileges"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hive WITH PASSWORD 'hive';
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
   \c metastore
-  \i /hive/hive-schema-postgres.sql
-  \i /hive/hive-txn-schema-postgres.sql
   \pset tuples_only
   \o /tmp/grant-privs
 SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO hive ;'
@@ -19,3 +17,7 @@ WHERE tableowner = CURRENT_USER and schemaname = 'public';
   \o
   \i /tmp/grant-privs
 EOSQL
+
+
+#   \i /hive/hive-schema-postgres.sql
+#  \i /hive/hive-txn-schema-postgres.sql
